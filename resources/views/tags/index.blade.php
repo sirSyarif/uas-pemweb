6d25783541f0749971af2ea12bdb8b15f4b2eff7
sirSyarif <arif09.sh@gmail.com>
2024-07-12 18:05:46 +0700

    fix: Remove show

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,37 +2,34 @@
 
 @section('content')
 
-	<div class="d-flex justify-content-end mb-3"><a href="{{ route('tags.create') }}" class="btn btn-info">Create</a></div>
+    <div class="d-flex justify-content-end mb-3"><a href="{{ route('tags.create') }}" class="btn btn-info">Create</a></div>
 
-	<table class="table table-bordered">
-		<thead>
-			<tr>
-				<th>id</th>
-				<th>name</th>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>name</th>
 
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($tags as $tag)
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tags as $tag)
+                <tr>
+                    <td>{{ $tag->id }}</td>
+                    <td>{{ $tag->name }}</td>
 
-				<tr>
-					<td>{{ $tag->id }}</td>
-					<td>{{ $tag->name }}</td>
-
-					<td>
-						<div class="d-flex gap-2">
-                            <a href="{{ route('tags.show', [$tag->id]) }}" class="btn btn-info">Show</a>
+                    <td>
+                        <div class="d-flex gap-2">
                             <a href="{{ route('tags.edit', [$tag->id]) }}" class="btn btn-primary">Edit</a>
-                            {!! Form::open(['method' => 'DELETE','route' => ['tags.destroy', $tag->id]]) !!}
-                                {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                            {!! Form::open(['method' => 'DELETE', 'route' => ['tags.destroy', $tag->id]]) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                             {!! Form::close() !!}
                         </div>
-					</td>
-				</tr>
-
-			@endforeach
-		</tbody>
-	</table>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @stop
